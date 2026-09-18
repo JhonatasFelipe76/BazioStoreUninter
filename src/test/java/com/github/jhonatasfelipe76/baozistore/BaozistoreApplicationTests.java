d8e8deb3235f1c0d70af3850e6f3246fea02534e
JhonatasFelipe76 <jhonatasestudos571@gmail.com>
2026-09-18 18:34:24 +0100

    Initial commit

---
+++ src/test/java/com/github/jhonatasfelipe76/baozistore/BaozistoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jhonatasfelipe76.baozistore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaozistoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
